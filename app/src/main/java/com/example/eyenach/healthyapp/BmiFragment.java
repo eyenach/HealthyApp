de2b78dd48dacac0f467d239e96b2a8f536a30be
eyenach <59070085@it.kmitl.ac.th>
2018-09-16 00:55:21 +0700

    complete slide_3

--- app/src/main/java/com/example/eyenach/healthyapp/BmiFragment.java
+++ app/src/main/java/com/example/eyenach/healthyapp/BmiFragment.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class BmiFragment extends Fragment {
 
@@ -14,5 +19,41 @@ public class BmiFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_bmi, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        initCalcBtn();
+    }
+
+    void initCalcBtn(){
+        Button _calcBtn = getView().findViewById(R.id.bmi_calc);
+        _calcBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText _height = getView().findViewById(R.id.bmi_height);
+                EditText _weight = getView().findViewById(R.id.bmi_weight);
+                TextView _result = getView().findViewById(R.id.bmi_bmi);
+
+                String _heightStr = _height.getText().toString();
+                String  _weightStr = _weight.getText().toString();
+                String _resultStr = _result.getText().toString();
+
+                if(_heightStr.isEmpty() || _weightStr.isEmpty()){
+                    Log.d("BMI", "FIELD NAME IS EMPTY");
+                    Toast.makeText(getActivity(), "กรุณาระบุข้อมูลให้ครบถ้วย", Toast.LENGTH_SHORT).show();
+                } else {
+                    float _heightFlo = Float.parseFloat(_heightStr)/100;
+                    float _weightFlo = Float.parseFloat(_weightStr);
+
+                    float _resultFlo = _weightFlo/(_heightFlo*_heightFlo);
+                    _result.setText(String.valueOf(_resultFlo));
+
+                    Log.d("BMI", "BMI IS VALUE");
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/eyenach/healthyapp/RegisterFragment.java
+++ app/src/main/java/com/example/eyenach/healthyapp/RegisterFragment.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterFragment extends Fragment {
 
@@ -14,5 +18,45 @@ public class RegisterFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        initRegister();
+    }
+
+    void initRegister(){
+        Button _regisBtn = getView().findViewById(R.id.regis_regis_btn);
+        _regisBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText _user = getView().findViewById(R.id.regis_user_id);
+                EditText _name = getView().findViewById(R.id.regis_name);
+                EditText _age = getView().findViewById(R.id.regis_age);
+                EditText _pass = getView().findViewById(R.id.regis_pass);
+
+                String _userStr = _user.getText().toString();
+                String _nameStr = _name.getText().toString();
+                String _ageStr = _age.getText().toString();
+                String _passStr = _pass.getText().toString();
+
+                if(_userStr.isEmpty() || _nameStr.isEmpty() || _ageStr.isEmpty() || _passStr.isEmpty()){
+                    Log.d("REGISTER", "FIELD NAME IS EMPTY");
+                    Toast.makeText(getActivity(), "กรุณาระบุข้อมูลให้ครบถ้วย", Toast.LENGTH_SHORT).show();
+                } else if(_userStr.equals("admin")){
+                    Log.d("REGISTER", "USER ALREADY EXIST");
+                    Toast.makeText(getActivity(), "user นี้มีอยู่ในระบบแล้ว", Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.d("REGISTER", "GOTO BMI");
+                    getActivity().getSupportFragmentManager()
+                            .beginTransaction()
+                            .addToBackStack(null)
+                            .replace(R.id.main_view, new BmiFragment())
+                            .commit();
+                }
+            }
+        });
     }
 }
