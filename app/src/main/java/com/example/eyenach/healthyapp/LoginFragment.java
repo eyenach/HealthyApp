c5a891a6818781641034e97d77cb651052256a8f
eyenach <59070085@it.kmitl.ac.th>
2018-09-16 18:58:15 +0700

    complete-login with email

--- app/src/main/java/com/example/eyenach/healthyapp/LoginFragment.java
+++ app/src/main/java/com/example/eyenach/healthyapp/LoginFragment.java
@@ -13,7 +13,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginFragment extends Fragment {
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Nullable
     @Override
@@ -34,23 +41,32 @@ public class LoginFragment extends Fragment {
         _loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText _user = getView().findViewById(R.id.login_user_id);
+                EditText _email = getView().findViewById(R.id.login_email);
                 EditText _pass = getView().findViewById(R.id.login_pass);
 
-                String _userStr = _user.getText().toString();
+                String _emailStr = _email.getText().toString();
                 String _passStr = _pass.getText().toString();
 
-                if(_userStr.isEmpty() || _passStr.isEmpty()){
+                if(_emailStr.isEmpty() || _passStr.isEmpty()){
                     Log.d("LOGIN", "USERNAME OR PASSWORD IS EMPTY");
                     Toast.makeText(getActivity(), "กรุณาระบุ username or password", Toast.LENGTH_SHORT).show();
-                } else if(_userStr.equals("admin") && _passStr.equals("admin")){
-                    getActivity().getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.main_view, new BmiFragment())
-                            .commit();
-                } else {
-                    Log.d("LOGIN", "INVALID USER OR PASSWORD");
-                    Toast.makeText(getActivity(), "username or password ไม่ถูกต้อง", Toast.LENGTH_SHORT).show();
+                }  else {
+                    Log.d("LOGIN", "LOGIN WITH EMAIL");
+                    mAuth.signInWithEmailAndPassword(_emailStr, _passStr).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            getActivity().getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .addToBackStack(null)
+                                    .replace(R.id.main_view, new BmiFragment())
+                                    .commit();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getActivity(), "กรุณาลงทะเบียนก่อนเข้าใช้งาน", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
             }
         });
